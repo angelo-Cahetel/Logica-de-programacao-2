b18ebf0a766be9a0ffe4d813891e837b09785141
angelo-Cahetel <angelo030825@gmail.com>
2024-06-10 05:35:53 -0300

    primeira commit

---
+++ app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+let paragrafo = document.querySelector('p');
+
+titulo.innerHTML = 'Jogo do número secreto';
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+
+function verificarChute() {
+    
+}
